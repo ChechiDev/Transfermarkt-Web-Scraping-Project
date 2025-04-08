d8a3cc702083c641896f359b984201785cd28d49
ChechiDev <chechidatadev@gmail.com>
2025-04-08 10:40:17 +0200

    Config: All Leagues scraping as POO

--- scraping/transfermarkt.py
+++ scraping/transfermarkt.py
@@ -3,6 +3,18 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 import time
+import re
+
+# Creamos una clase para la configuración de las ligas
+class Leagues:
+    def __init__(self, id_league, league_name, league_url):
+        self.id_league = id_league
+        self.league_name = league_name
+        self.league_url = league_url
+
+    def __repr__(self):
+        return f"League(id_league='{self.id_league}', league_name='{self.league_name}, league_url='{self.league_url}')"
+
 
 # Creamos una clase que defina a Transfermarkt:
 class TransfermarktScraper:
@@ -22,20 +34,66 @@ class TransfermarktScraper:
             return None
 
 
+    def get_all_leagues(self, soup):
+        leagues_list = []
+
+        # Encontrar todos los <a> dentro de la tabla que contengan las ligas
+        league_links = soup.select('table.items a')
+
+        for link in league_links:
+            league_name = link.get_text(strip=True)
+
+            league_url = link.get('href')
+            # Extraemos el ID de la URL:
+            id_league = league_url.split("/")[-1]
+
+            # Filtramos para evitar información innecesaria:
+            if(
+                not league_name or
+                re.match(r"[\d%]", league_name) or
+                league_name in ["Player", "Avg. age", "Foreigners", "Goals per match", "Average market value", "Total value"]
+
+            ): continue
+
+            # Creamos el objeto League:
+            league = Leagues(
+                id_league=id_league,
+                league_name=league_name,
+                league_url=f"https://www.transfermarkt.com{league_url}" # <- URL completa
+            )
+
+            # Creamos un diccionario con las ligas:
+            league_dict = {
+                "id_league": league.id_league,
+                "league_name": league.league_name,
+                "league_url": league.league_url
+            }
+            # Añadimos el diccionario a la lista:
+            leagues_list.append(league_dict)
+
+        # Convertimos a df:
+        df_leagues = pd.DataFrame(leagues_list)
+
+        return df_leagues
+
+
+
+
 
 # Comprobamos:
-# -------------------------------------------------------------------------------------------
-if __name__== "__main__":
-    # Creamos instancia del Scraper:
+# ----------------------------------------------------------------------------------------------
+if __name__ == "__main__":
     scraper = TransfermarktScraper()
-
-    # URL de prueba
-    url = "https://www.transfermarkt.com/laliga/startseite/wettbewerb/ES1"
-
-    # Hacemos una petición
+    url = "https://www.transfermarkt.com/wettbewerbe/europa/wettbewerbe?plus=1"
     soup = scraper.get_req_to_soup(url)
 
     if soup:
-        print("Página cargada!")
+        print("✅ Página cargada correctamente.")
+        leagues = scraper.get_all_leagues(soup)
+
+        # Mostramos con pandas:
+        df = pd.DataFrame(leagues)
+        print(df)
+
     else:
-        print("Error al cargar la página")
+        print("❌ Error al cargar la página.")
